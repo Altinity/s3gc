34a1a20f39bbee53a1365c00245959946afd9397
Ilya Golshtein <igolshtein@altinity.com>
2024-05-06 17:38:39 +0300

    flags command line/environment interoperability

--- s3gc.py
+++ s3gc.py
@@ -15,6 +15,7 @@ It is possible to split these stages or do everything at one go.
 """
 
 import os
+import sys
 from io import StringIO
 from minio import Minio
 from minio.deleteobjects import DeleteObject
@@ -107,7 +108,16 @@ parser.add_argument(
 parser.add_argument(
     "--s3secure",
     "--s3-secure",
-    dest="s3secure",
+    action="store_true",
+    dest="s3secure_flag",
+    default=False,
+    help="S3 secure mode"
+)
+parser.add_argument(
+    "--s3secureflag",
+    "--s3-secure-flag",
+    type=bool,
+    dest="s3secure_flag",
     default=False,
     help="S3 secure mode"
 )
@@ -129,7 +139,15 @@ parser.add_argument(
     "--keepdata",
     "--keep-data",
     action="store_true",
-    dest="keepdata",
+    dest="keepdata_flag",
+    default=False,
+    help="keep auxiliary data in ClickHouse table",
+)
+parser.add_argument(
+    "--keepdataflag",
+    "--keep-data-flag",
+    type=bool,
+    dest="keepdata_flag",
     default=False,
     help="keep auxiliary data in ClickHouse table",
 )
@@ -137,7 +155,15 @@ parser.add_argument(
     "--collectonly",
     "--collect-only",
     action="store_true",
-    dest="collectonly",
+    dest="collectonly_flag",
+    default=False,
+    help="put object names to auxiliary table",
+)
+parser.add_argument(
+    "--collectonlyflag",
+    "--collect-only-flag",
+    type=bool,
+    dest="collectonly_flag",
     default=False,
     help="put object names to auxiliary table",
 )
@@ -145,7 +171,15 @@ parser.add_argument(
     "--usecollected",
     "--use-collected",
     action="store_true",
-    dest="usecollected",
+    dest="usecollected_flag",
+    default=False,
+    help="auxiliary data is already collected in ClickHouse table",
+)
+parser.add_argument(
+    "--usecollectedflag",
+    "--use-collected-flag",
+    type=bool,
+    dest="usecollected_flag",
     default=False,
     help="auxiliary data is already collected in ClickHouse table",
 )
@@ -216,21 +250,45 @@ parser.add_argument(
 parser.add_argument(
     "--verbose",
     action="store_true",
-    dest="verbose",
+    dest="verbose_flag",
+    default=False,
+    help="debug output"
+)
+parser.add_argument(
+    "--verboseflag",
+    "--verbose-flag",
+    type=bool,
+    dest="verbose_flag",
     default=False,
     help="debug output"
 )
 parser.add_argument(
     "--debug",
     action="store_true",
-    dest="debug",
+    dest="debug_flag",
+    default=False,
+    help="trace output (more verbose)",
+)
+parser.add_argument(
+    "--debugflag",
+    "--debug-flag",
+    type=bool,
+    dest="debug_flag",
     default=False,
     help="trace output (more verbose)",
 )
 parser.add_argument(
     "--silent",
     action="store_true",
-    dest="silent",
+    dest="silent_flag",
+    default=False,
+    help="no log"
+)
+parser.add_argument(
+    "--silentflag",
+    "--silent-flag",
+    dest="silent_flag",
+    type=bool,
     default=False,
     help="no log"
 )
@@ -264,7 +322,8 @@ if args.listoptions:
     for ln in f.getvalue().splitlines():
 
         (key, value) = ln.split(': ')
-        if (key == 'listoptions'):
+        # if (key in ['listoptions', 'verbose', 'debug', 'silent', 'keepdata', 'collectonly', 'usecollected']):
+        if (key in ['listoptions']):
             continue
         if backslash:
             print(" \\ ")
@@ -276,15 +335,32 @@ if args.listoptions:
     exit()
 
 
-logging.getLogger().setLevel(logging.WARNING)
-if args.verbose:
-    logging.getLogger().setLevel(logging.INFO)
-if args.debug:
-    logging.getLogger().setLevel(logging.DEBUG)
-if args.silent:
-    logging.getLogger().setLevel(logging.CRITICAL)
 
-logging.info(
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO) # set logger level
+# logger.setLevel(logging.WARNING)
+if args.verbose_flag:
+    print("verbose")
+    logger.setLevel(logging.INFO)
+if args.debug_flag:
+    print("debug")
+    logger.setLevel(logging.DEBUG)
+if args.silent_flag:
+    print("silent")
+    logger.setLevel(logging.CRITICAL)
+
+
+logFormatter = logging.Formatter\
+("%(asctime)s %(levelname)s %(message)s")
+consoleHandler = logging.StreamHandler(sys.stdout) #set streamhandler to stdout
+consoleHandler.setFormatter(logFormatter)
+logger.addHandler(consoleHandler)
+
+
+
+
+logger.info(
     f"Connecting to ClickHouse, host={args.chhost}, port={args.chport}, username={args.chuser}, password={args.chpass}"
 )
 ch_client = clickhouse_connect.get_client(
@@ -294,27 +370,27 @@ ch_client = clickhouse_connect.get_client(
     password=args.chpass,
 )
 
-if args.s3secure:
-    logging.debug(f"using SSL certificate {args.s3sslcertfile}")
+if args.s3secure_flag:
+    logger.debug(f"using SSL certificate {args.s3sslcertfile}")
     os.environ["SSL_CERT_FILE"] = args.s3sslcertfile
 
 tname = f"{args.collecttableprefix}{args.s3diskname}"
 
-if not args.usecollected:
-    logging.info(
-        f"Connecting to S3, host:port={args.s3ip}:{args.s3port}, access_key={args.s3accesskey}, secret_key={args.s3secretkey}, secure={args.s3secure}"
+if not args.usecollected_flag:
+    logger.info(
+        f"Connecting to S3, host:port={args.s3ip}:{args.s3port}, access_key={args.s3accesskey}, secret_key={args.s3secretkey}, secure={args.s3secure_flag}"
     )
     minio_client = Minio(
         f"{args.s3ip}:{args.s3port}",
         access_key=args.s3accesskey,
         secret_key=args.s3secretkey,
-        secure=args.s3secure,
+        secure=args.s3secure_flag,
         http_client=urllib3.PoolManager(cert_reqs="CERT_NONE"),
     )
 
     objects = minio_client.list_objects(args.s3bucket, "data/", recursive=True, start_after=args.collectafter)
 
-    logging.info(f"creating {tname}")
+    logger.info(f"creating {tname}")
     ch_client.command(
         f"CREATE TABLE IF NOT EXISTS {tname} (objpath String, active Bool) ENGINE ReplacingMergeTree ORDER BY objpath PARTITION BY CRC32(objpath) % {args.samples}"
     )
@@ -336,14 +412,14 @@ if not args.usecollected:
             rest_row_nums -= len(objs)
             if rest_row_nums == 0 or go_on == False:
                 go_on = False
-                if not args.silent:
+                if not args.silent_flag:
                     if len(objs):
                         print(f"s3gc: {objs[-1]}")
                     else:
                         print(f"s3gc: No object")
                 break
 
-if not args.collectonly:
+if not args.collectonly_flag:
     srdp = "system.remote_data_paths"
     if args.clustername:
         srdp = f"clusterAllReplicas({args.clustername}, {srdp})"
@@ -354,10 +430,10 @@ if not args.collectonly:
     except Exception:
         pass
     if num_rows==0:
-        logging.info(
+        logger.info(
             f"auxiliary table {tname} does not exist or empty, nothing to do"
         )
-        if not args.silent:
+        if not args.silent_flag:
             print("s3gc: OK")
 
         exit()
@@ -372,18 +448,18 @@ if not args.collectonly:
         (rdp.remote_path = s3o.objpath AND rdp.disk_name='{args.s3diskname}')
         WHERE CRC32(s3o.objpath) % {args.samples} = {sample} AND s3o.active=true
         ORDER BY s3o.objpath  SETTINGS final = 1"""
-        logging.info(antijoin)
+        logger.info(antijoin)
 
         with ch_client.query_row_block_stream(antijoin) as stream:
             for block in stream:
                 objects_to_remove=[]
                 for row in block:
-                    logging.debug(f"removing {row[0]}")
+                    logger.debug(f"removing {row[0]}")
                     objects_to_remove.append(DeleteObject(row[0]))
                     objects_to_remove.append(DeleteObject(row[0] + 'ss'))
                     errors = minio_client.remove_objects(args.s3bucket, objects_to_remove)
                     for error in errors:
-                        logging.info(f"error occurred when deleting object {error}")
+                        logger.info(f"error occurred when deleting object {error}")
 
                     num_removed += len(objects_to_remove)
                     objs.append([row[0], False])
@@ -391,11 +467,11 @@ if not args.collectonly:
         ch_client.insert(tname, objs, column_names=["objpath", "active"])
 
 
-    logging.info(f"{num_removed} objects are removed")
+    logger.info(f"{num_removed} objects are removed")
 
-    if not args.keepdata:
-        logging.info(f"truncating {tname}")
+    if not args.keepdata_flag:
+        logger.info(f"truncating {tname}")
         ch_client.command(f"TRUNCATE TABLE {tname}")
 
-if not args.silent:
+if not args.silent_flag:
     print("s3gc: OK")
